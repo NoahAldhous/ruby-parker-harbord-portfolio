71a8698404a6ef60ec1122d8a0a9a55c37817201
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-14 14:40:59 +0100

    customised background color palette by editing them.backgroundColor section of Tailwind Config file

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
       <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
         <p className="font-superRetro italic text-2xl">
           Ruby Parker-Harbord
