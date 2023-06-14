f5fa1bbbdedd355614773aad88214a7f593db3b7
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-14 14:56:03 +0100

    included next/babel within eslintrc.json to remove error in tailwind.config file. Issue was connected to having prettier extension within the project

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -2,11 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
       <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-        <p className="font-superRetro italic text-2xl">
+        <p className="font-superRetro italic text-lg sm:text-2xl">
           Ruby Parker-Harbord
         </p>
-        <div className="font-mono fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          Hello world!
+        <div className="font-mono">
         </div>
       </div>
     </main>
