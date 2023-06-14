09c24cc32b791d22bab81f4008e0ac504a52b1cd
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-14 15:00:22 +0100

    added break point for text size for header

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary">
       <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-        <p className="font-superRetro italic text-lg sm:text-2xl">
+        <p className="font-superRetro italic text-lg md:text-3xl">
           Ruby Parker-Harbord
         </p>
         <div className="font-mono">
