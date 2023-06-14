5a9c53d1cd7509edf04e53e19f86447f006d930c
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-14 14:29:04 +0100

    installed Super Retro M54 font locally, and edited the layout.tsx file to configure the local font as a variable to be used by TailwindCSS

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -1,7 +1,20 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local';
 
-const inter = Inter({ subsets: ['latin'] })
+const superRetro = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Super-Retro-M54.ttf',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/Super-Retro-Italic-M54.ttf',
+      style: 'italic'
+    },
+  ],
+  variable: '--superRetro'
+})
+
 
 export const metadata = {
   title: 'Ruby Parker-Harbord',
@@ -15,7 +28,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={superRetro.variable}>{children}</body>
     </html>
   )
 }
