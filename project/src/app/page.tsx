8e7e6e3f716e5c441c88beb76074eea56e17ba9d
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-15 10:40:00 +0100

    styled navigation bar in a basic sense, added breakpoints so on phone screens it will become hidden.

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-primary dark:bg-dark">
-      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-        <p className="font-superRetro italic text-lg md:text-3xl">
+    <main className="flex min-h-screen flex-col items-center p-24 bg-primary dark:bg-dark">
+      <div className="hidden sm:block w-full bg-red-400 top-0 absolute z-10">
+        <p className="font-superRetro italic text-md md:text-3xl">
           Ruby Parker-Harbord
         </p>
         <div className="font-mono">
