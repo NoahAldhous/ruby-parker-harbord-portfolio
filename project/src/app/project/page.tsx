049843354a406cd6e770f231aded33c2ec297d29
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-20 16:19:06 +0100

    changed all occurances of md: in projects route to sm: for proper styling on landscape phone screens

--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -5,32 +5,32 @@ import portrait1 from '../../assets/images/portrait1.jpg';
 import wedding1 from '../../assets/images/wedding1.jpg'
 
 export default function Page(){
-    return <main className="flex min-h-screen min-w-screen md:h-fit flex-col items-center pt-24 bg-primary dark:bg-dark">
-     <div className="md:h-1/2 font-superRetro italic flex flex-col md:flex-row md:items-start items-center w-full">
-        <div className="md:w-1/3 md:relative flex flex-col items-center justify-between bg-zinc-300 py-8 px-6">
-            <Link className="flex flex-col items-center md:justify-center w-full cursor-pointer" href='/project/portraits'>
+    return <main className="flex min-h-screen min-w-screen sm:h-fit flex-col items-center pt-24 bg-primary dark:bg-dark">
+     <div className="sm:h-1/2 font-superRetro italic flex flex-col sm:flex-row sm:items-start items-center w-full">
+        <div className="sm:w-1/3 sm:relative flex flex-col items-center justify-between bg-zinc-300 py-8 px-6">
+            <Link className="flex flex-col items-center sm:justify-center w-full cursor-pointer" href='/project/portraits'>
                 <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
-                <div className="hidden md:tracking-wider md:text-slate-300 md:absolute md:min-w-full md:min-h-full md:flex md:justify-center md:items-center md:bg-slate-900 md:opacity-0 md:hover:opacity-90 md:transition md:duration-200">Portraits</div>
+                <div className="hidden sm:tracking-wider sm:text-slate-300 sm:absolute sm:min-w-full sm:min-h-full sm:flex sm:justify-center sm:items-center sm:bg-slate-900 sm:opacity-0 sm:hover:opacity-90 sm:transition sm:duration-200">Portraits</div>
             </Link>
-            <div className="pt-4 md:hidden">
+            <div className="pt-4 sm:hidden">
                 Portraits
             </div>
         </div>
-        <div className="md:w-1/3 md:relative flex flex-col items-center justify-between bg-zinc-400 py-8 px-6">
-            <Link className="flex flex-col items-center md:justify-center w-full cursor-pointer" href='/project/headshots'>
+        <div className="sm:w-1/3 sm:relative flex flex-col items-center justify-between bg-zinc-400 py-8 px-6">
+            <Link className="flex flex-col items-center sm:justify-center w-full cursor-pointer" href='/project/headshots'>
                 <Image placeholder='blur' src={portrait1} alt='woman drinking from glass'></Image>
-                <div className="hidden md:tracking-wider md:text-slate-300 md:absolute md:min-w-full md:min-h-full md:flex md:justify-center md:items-center md:bg-slate-900 md:opacity-0 md:hover:opacity-90 md:transition md:duration-200">Headshots</div>
+                <div className="hidden sm:tracking-wider sm:text-slate-300 sm:absolute sm:min-w-full sm:min-h-full sm:flex sm:justify-center sm:items-center sm:bg-slate-900 sm:opacity-0 sm:hover:opacity-90 sm:transition sm:duration-200">Headshots</div>
             </Link>
-            <div className="pt-4 md:hidden">
+            <div className="pt-4 sm:hidden">
                 Headshots
             </div>
         </div>
-        <div className="md:w-1/3 md:relative flex flex-col items-center justify-between bg-zinc-200 py-8 px-6">
-            <Link className="flex flex-col items-center md:justify-center w-full cursor-pointer" href='/project/weddings'>
+        <div className="sm:w-1/3 sm:relative flex flex-col items-center justify-between bg-zinc-200 py-8 px-6">
+            <Link className="flex flex-col items-center sm:justify-center w-full cursor-pointer" href='/project/weddings'>
                 <Image placeholder='blur' src={wedding1} alt='woman drinking from glass'></Image>
-                <div className="hidden md:tracking-wider md:text-slate-300 md:absolute md:min-w-full md:min-h-full md:flex md:justify-center md:items-center md:bg-slate-900 md:opacity-0 md:hover:opacity-90 md:transition md:duration-200">Weddings</div>
+                <div className="hidden sm:tracking-wider sm:text-slate-300 sm:absolute sm:min-w-full sm:min-h-full sm:flex sm:justify-center sm:items-center sm:bg-slate-900 sm:opacity-0 sm:hover:opacity-90 sm:transition sm:duration-200">Weddings</div>
             </Link>
-            <div className="pt-4 md:hidden">
+            <div className="pt-4 sm:hidden">
                 Weddings
             </div>
         </div>
