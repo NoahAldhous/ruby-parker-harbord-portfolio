34ac3b0486e02b9d223cc3b60c7dd758558d9268
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-20 13:52:50 +0100

    created text that appears over images on hover in /project for larger screen sizes

--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -5,20 +5,35 @@ import portrait1 from '../../assets/images/portrait1.jpg';
 import wedding1 from '../../assets/images/wedding1.jpg'
 
 export default function Page(){
-    return <main className="flex min-h-screen flex-col items-center pt-24 bg-primary dark:bg-dark">
-     <div className="flex flex-col items-center bg-red-500 w-full cursor-pointer">
-        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-500 p-6" href='/project/portraits'>
-            <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
-        </Link>
-            <div>Portraits</div>
-        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-300 p-6" href='/project/headshots'>
-            <Image placeholder='blur' src={portrait1} alt='woman drinking from glass'></Image>
-        </Link>
-            <div>Headshots</div>
-        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-400 p-6" href='/project/weddings'>
-            <Image placeholder='blur' src={wedding1} alt='woman drinking from glass'></Image>
-        </Link>
-            <div>Weddings</div>
+    return <main className="flex min-h-screen min-w-screen md:h-fit flex-col items-center pt-24 bg-primary dark:bg-dark">
+     <div className="md:h-1/2 font-superRetro italic flex flex-col md:flex-row md:items-start items-center w-full">
+        <div className="md:w-1/3 md:relative flex flex-col items-center justify-between bg-zinc-300 py-8 px-6">
+            <Link className="flex flex-col items-center md:justify-center w-full cursor-pointer" href='/project/portraits'>
+                <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
+                <div className="hidden md:tracking-wider md:text-slate-300 md:absolute md:min-w-full md:min-h-full md:flex md:justify-center md:items-center md:bg-slate-900 md:opacity-0 md:hover:opacity-90 md:transition md:duration-200">Portraits</div>
+            </Link>
+            <div className="pt-4 md:hidden">
+                Portraits
+            </div>
+        </div>
+        <div className="md:w-1/3 md:relative flex flex-col items-center justify-between bg-zinc-400 py-8 px-6">
+            <Link className="flex flex-col items-center md:justify-center w-full cursor-pointer" href='/project/headshots'>
+                <Image placeholder='blur' src={portrait1} alt='woman drinking from glass'></Image>
+                <div className="hidden md:tracking-wider md:text-slate-300 md:absolute md:min-w-full md:min-h-full md:flex md:justify-center md:items-center md:bg-slate-900 md:opacity-0 md:hover:opacity-90 md:transition md:duration-200">Headshots</div>
+            </Link>
+            <div className="pt-4 md:hidden">
+                Headshots
+            </div>
+        </div>
+        <div className="md:w-1/3 md:relative flex flex-col items-center justify-between bg-zinc-200 py-8 px-6">
+            <Link className="flex flex-col items-center md:justify-center w-full cursor-pointer" href='/project/weddings'>
+                <Image placeholder='blur' src={wedding1} alt='woman drinking from glass'></Image>
+                <div className="hidden md:tracking-wider md:text-slate-300 md:absolute md:min-w-full md:min-h-full md:flex md:justify-center md:items-center md:bg-slate-900 md:opacity-0 md:hover:opacity-90 md:transition md:duration-200">Weddings</div>
+            </Link>
+            <div className="pt-4 md:hidden">
+                Weddings
+            </div>
+        </div>
      </div>
     </main>
 }
