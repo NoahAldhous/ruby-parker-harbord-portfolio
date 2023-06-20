9fdd8e50d4ce2638866d9e4d5e006653c4f0e7e3
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-20 12:20:57 +0100

    commented out imports of the images that were throwing errors in deployment

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image';
 import placeholderMiriam from '../assets/images/placeholder-miriam.jpg'
-import placeholderIceCream from '../assets/images/placeholder-iceCream.jpg'
-import placeholderRed from '../assets/images/placeholder-red.jpg'
+// import placeholderIceCream from '../assets/images/placeholder-iceCream.jpg'
+// import placeholderRed from '../assets/images/placeholder-red.jpg'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center md:p-24 md:pt-32 p-10 pt-24 bg-primary dark:bg-dark">
       <div className="w-fill object-contain flex flex-col md:grid gap-6 md:gap-4 md:grid-cols-3 md:grid-rows-3">
-        <Image placeholder='blur' src={placeholderIceCream} alt="two people eating ice cream"></Image>
-        <Image placeholder='blur' src={placeholderRed} alt="Two women in red light"></Image>
+        {/* <Image placeholder='blur' src={placeholderIceCream} alt="two people eating ice cream"></Image>
+        <Image placeholder='blur' src={placeholderRed} alt="Two women in red light"></Image> */}
         <Image placeholder='blur' src={placeholderMiriam} alt="Young woman drinking from a glass"></Image>
-        <Image placeholder='blur' src={placeholderRed} alt="Two women in red light"></Image>
+        {/* <Image placeholder='blur' src={placeholderRed} alt="Two women in red light"></Image> */}
         <Image placeholder='blur' src={placeholderMiriam} alt="Young woman drinking from a glass"></Image>
       </div>
     </main>
