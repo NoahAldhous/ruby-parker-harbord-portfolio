bcb9fdf012bfe254736b9e651d9b208f78c29ba4
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-20 12:43:45 +0100

    added wedding and headshots routes under projects

--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -1,22 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import placeholderMiriam from '../../assets/images/placeholder-miriam.jpg';
+import portrait1 from '../../assets/images/portrait1.jpg';
+import wedding1 from '../../assets/images/wedding1.jpg'
 
 export default function Page(){
     return <main className="flex min-h-screen flex-col items-center pt-24 bg-primary dark:bg-dark">
      <div className="flex flex-col items-center bg-red-500 w-full cursor-pointer">
-        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-500 p-6" href='/project/beer'>
+        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-500 p-6" href='/project/portraits'>
             <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
         </Link>
-            <div>Beer</div>
-        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-300 p-6" href='/project/beer'>
-            <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
+            <div>Portraits</div>
+        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-300 p-6" href='/project/headshots'>
+            <Image placeholder='blur' src={portrait1} alt='woman drinking from glass'></Image>
         </Link>
-            <div>Wine</div>
-        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-400 p-6" href='/project/beer'>
-            <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
+            <div>Headshots</div>
+        <Link className="flex flex-col items-center pt-8 w-full bg-zinc-400 p-6" href='/project/weddings'>
+            <Image placeholder='blur' src={wedding1} alt='woman drinking from glass'></Image>
         </Link>
-            <div>Cider</div>
+            <div>Weddings</div>
      </div>
     </main>
 }
