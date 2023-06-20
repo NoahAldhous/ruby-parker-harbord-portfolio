35942a6f8d295a47fbd6ee3f45ceeb29c42a13e8
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-20 11:48:48 +0100

    added placeholder image in projects route to begin building lightbox gallery layout

--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -1,5 +1,11 @@
+import Image from 'next/image'
+import placeholderMiriam from '../../../public/static/images/placeholder-miriam.jpg'
+
 export default function Page(){
-    return <main className="flex min-h-screen flex-col items-center p-24 bg-primary dark:bg-dark">
-     <h1>Welcome to projects!</h1>
+    return <main className="flex min-h-screen flex-col items-center pt-24 bg-primary dark:bg-dark">
+     <div className="flex flex-col items-center p-6 bg-red-500 w-full cursor-pointer">
+        <Image placeholder='blur' src={placeholderMiriam} alt='woman drinking from glass'></Image>
+        <div>Beer</div>
+     </div>
     </main>
 }
