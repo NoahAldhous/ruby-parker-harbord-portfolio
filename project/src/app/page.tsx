aa668d8e5e141a13acca8647c2c7e53470a0adc2
NoahAldhous <naldhous@hotmail.co.uk>
2023-06-20 11:27:27 +0100

    moved placeholder pictures into public/static/images. this may potentially resolve an issue at deployment

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
-import placeholderMiriam from '../../public/pictures/placeholder-miriam.jpg'
-import placeholderIceCream from '../../public/pictures/placeholder-iceCream.jpg'
-import placeholderRed from '../../public/pictures/placeholder-red.jpg'
+import placeholderMiriam from '../../public/static/images/placeholder-miriam.jpg'
+import placeholderIceCream from '../../public/static/images/placeholder-iceCream.jpg'
+import placeholderRed from '../../public/static/images/placeholder-red.jpg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center md:p-24 p-10 pt-24 bg-primary dark:bg-dark">
+    <main className="flex min-h-screen flex-col items-center md:p-24 md:pt-32 p-10 pt-24 bg-primary dark:bg-dark">
       <div className="w-fill object-contain flex flex-col md:grid gap-6 md:gap-4 md:grid-cols-3 md:grid-rows-3">
         <Image placeholder='blur' src={placeholderIceCream} alt="two people eating ice cream"></Image>
         <Image placeholder='blur' src={placeholderRed} alt="Two women in red light"></Image>
