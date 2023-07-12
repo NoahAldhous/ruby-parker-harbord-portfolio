f69aad655221950dd34c0058a3407c9d7f2c9e0f
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-12 22:52:44 +0100

    removed transition components as they were not animating. need to investigate why they are not displaying

--- project/src/app/contact/page.tsx
+++ project/src/app/contact/page.tsx
@@ -1,27 +1,13 @@
-'use client';
-
 import Link from 'next/link';
 import ContactLink from '../components/ContactLink';
-import { Transition } from '@headlessui/react'
 
 export default function Page(){
     return <main className="flex flex-col space-y-8 sm:space-y-10 justify-center items-start space-around h-screen w-screen p-24 bg-primary dark:bg-dark">
-        <Transition 
-          show={true}
-          appear={true}
-          enter="transition-opacity duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-all ease-in-out duration-300"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
         <ContactLink 
             firstLineText='Follow me on'
             secondLineText='Instagram'
             url='https://www.instagram.com/rubyparkerh/'
         />
-        </Transition>
         <ContactLink 
             firstLineText='Send me an'
             secondLineText='Email'
--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -10,9 +10,6 @@ import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 
-//import headless UI transitions
-import { Transition } from '@headlessui/react'
-
 // import plugins
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import lgZoom from 'lightgallery/plugins/zoom';
@@ -34,36 +31,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center md:p-24 md:pt-32 p-10 pt-24 bg-primary dark:bg-dark">
       <div className="w-fill object-contain flex flex-col sm:grid gap-6 sm:gap-4 sm:grid-cols-3 sm:grid-rows-3">
-        <Transition 
-          show={true}
-          appear={true}
-          enter="transition-opacity duration-500 delay-[200ms]"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-all ease-in-out duration-300"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
         <Image src={portrait1} alt='img-1'/>
-        </Transition>
-        <Transition 
-          show={true}
-          appear={true}
-          enter="transition-opacity duration-75"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-        >
         <Image src={portrait2} alt='img-1'/>
-        </Transition>
-        <Transition 
-          show={true}
-          appear={true}
-          enter="transition-opacity duration-75"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-        >
         <Image src={localfile} alt='img-1'/>
-        </Transition>
       </div>
     </main>
   )
