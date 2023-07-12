0d8e342e99cafbda6d2de3ff32fc76f93a5e647d
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-12 18:40:01 +0100

    changed font sizing of contact pages at 3 main breakpoints

--- project/src/app/components/ContactLink.tsx
+++ project/src/app/components/ContactLink.tsx
@@ -8,7 +8,7 @@ interface ContactLinkProps {
 
 export default function ContactLink({firstLineText, secondLineText, url}: ContactLinkProps){
     return <div>    
-        <p className="text-sm sm:text-lg">{firstLineText}</p>
-        <Link className="font-superRetro italic underline text-md md:text-3xl align-middle tracking-wider transition-all duration-600 sm:hover:text-slate-600 sm:hover:pl-2" href={url} target="_blank">{secondLineText}</Link>
+        <p className="text-sm sm:text-lg md:text-2xl">{firstLineText}</p>
+        <Link className="font-superRetro italic underline text-md sm:text-3xl md:text-6xl align-middle tracking-wider transition-all duration-600 sm:hover:text-slate-600 sm:hover:pl-2" href={url} target="_blank">{secondLineText}</Link>
     </div>
 }
