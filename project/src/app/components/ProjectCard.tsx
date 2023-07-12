6707738d1e9f3c70631762dee9720d81bd176fff
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-12 21:39:55 +0100

    fixed small error where the size of images meant to be applied to phone screens was being applied to desktop screen sizes.

--- project/src/app/components/ProjectCard.tsx
+++ project/src/app/components/ProjectCard.tsx
@@ -9,7 +9,7 @@ interface ProjectCardProps {
 
 export default function ProjectCard({imageSource, projectTitle, imageAlt}:ProjectCardProps){
     return <div className="sm:w-full sm:h-full h-screen w-screen flex flex-col relative items-center sm:justify-between p-2 sm:p-0">
-        <Link className="group sm:mb-4 mb-1 h-2/3 flex flex-col relative items-center sm:justify-center w-full  cursor-pointer" href={`/project/${projectTitle}`}>
+        <Link className="group sm:mb-4 mb-1 h-2/3 sm:h-full flex flex-col relative items-center sm:justify-center w-full cursor-pointer" href={`/project/${projectTitle}`}>
             <div className="relative flex sm:inline-block object-contain h-full w-full">
                 <Image placeholder='blur' className="object-contain sm:group-hover:opacity-60 sm:transition sm:duration-200" src={imageSource} alt={imageAlt} fill={true}/> 
                 <div className="hidden sm:tracking-wider sm:text-lg md:text-3xl sm:text-slate-800 sm:absolute sm:w-full sm:h-full sm:flex sm:justify-center sm:items-center sm:opacity-0 sm:group-hover:opacity-100 sm:transition sm:duration-200">
