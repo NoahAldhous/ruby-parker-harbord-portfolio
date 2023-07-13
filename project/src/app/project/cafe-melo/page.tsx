d4dbc79c85d0e3770fb4b067f79015fa395cfa39
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-13 14:15:12 +0100

    renamed photos in cafe-melo folder for easier usage

--- project/src/app/project/cafe-melo/page.tsx
+++ project/src/app/project/cafe-melo/page.tsx
@@ -1,3 +1,5 @@
+import Masonry from 'react-masonry-css';
+
 export default function Page(){
     return <main className="flex min-h-screen flex-col items-center pt-24 px-6 bg-primary dark:bg-dark">
      <div className="flex flex-col items-center p-6 bg-red-500 w-full cursor-pointer">
