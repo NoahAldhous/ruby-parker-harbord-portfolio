2d47330c1752155a62626c986631296616e0bf76
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-13 15:05:25 +0100

    masonry grid properly displaying images for Cafe Melo project path

--- project/src/app/project/cafe-melo/page.tsx
+++ project/src/app/project/cafe-melo/page.tsx
@@ -1,9 +1,44 @@
+'use client';
+
 import Masonry from 'react-masonry-css';
+import Image from 'next/image';
+
+//image imports
+import melo1 from '../../../assets/images/projects/cafe-melo/melo1.jpg';
+import melo2 from '../../../assets/images/projects/cafe-melo/melo2.jpg';
+import melo3 from '../../../assets/images/projects/cafe-melo/melo3.jpg';
+import melo4 from '../../../assets/images/projects/cafe-melo/melo4.jpg';
+import melo5 from '../../../assets/images/projects/cafe-melo/melo5.jpg';
+import melo6 from '../../../assets/images/projects/cafe-melo/melo6.jpg';
+import melo7 from '../../../assets/images/projects/cafe-melo/melo7.jpg';
+import melo8 from '../../../assets/images/projects/cafe-melo/melo8.jpg';
+import melo9 from '../../../assets/images/projects/cafe-melo/melo9.jpg';
+import melo10 from '../../../assets/images/projects/cafe-melo/melo10.jpg';
+import melo11 from '../../../assets/images/projects/cafe-melo/melo11.jpg';
+import melo12 from '../../../assets/images/projects/cafe-melo/melo12.jpg';
+import melo13 from '../../../assets/images/projects/cafe-melo/melo13.jpg';
+import melo14 from '../../../assets/images/projects/cafe-melo/melo14.jpg';
+
 
 export default function Page(){
     return <main className="flex min-h-screen flex-col items-center pt-24 px-6 bg-primary dark:bg-dark">
-     <div className="flex flex-col items-center p-6 bg-red-500 w-full cursor-pointer">
-        <div>Cafe Melo</div>
+     <div className="flex flex-col items-center p-12 border-2 border-red-200 w-full cursor-pointer">
+        <Masonry breakpointCols={3} className="flex gap-8" columnClassName="">
+            <Image placeholder="blur" src={melo1} alt='melo1' className="mb-8"/>
+            <Image placeholder="blur" src={melo2} alt='melo2' className="mb-8"/>
+            <Image placeholder="blur" src={melo3} alt='melo3' className="mb-8"/>
+            <Image placeholder="blur" src={melo4} alt='melo4' className="mb-8"/>
+            <Image placeholder="blur" src={melo5} alt='melo5' className="mb-8"/>
+            <Image placeholder="blur" src={melo6} alt='melo6' className="mb-8"/>
+            <Image placeholder="blur" src={melo7} alt='melo7' className="mb-8"/>
+            <Image placeholder="blur" src={melo8} alt='melo8' className="mb-8"/>
+            <Image placeholder="blur" src={melo9} alt='melo9' className="mb-8"/>
+            <Image placeholder="blur" src={melo10} alt='melo10' className="mb-8"/>
+            <Image placeholder="blur" src={melo11} alt='melo11' className="mb-8"/>
+            <Image placeholder="blur" src={melo12} alt='melo12' className="mb-8"/>
+            <Image placeholder="blur" src={melo13} alt='melo13' className="mb-8"/>
+            <Image placeholder="blur" src={melo14} alt='melo14' className="mb-8"/>
+        </Masonry>
      </div>
     </main>
 }
--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -3,13 +3,13 @@ import portrait1 from '../../assets/images/portrait1.jpg';
 import ProjectCard from '../components/ProjectCard';
 
 //image imports
-import cafeMeloThumb from '../../assets/images/projects/cafe-melo/1 and Thumbnail.jpg';
+import cafeMeloThumb from '../../assets/images/projects/cafe-melo/melo1.jpg';
 import engagementsThumb from '../../assets/images/projects/engagements/DSCF6970.jpg';
 import headshotsThumb from '../../assets/images/projects/headshots/DSCF6699.jpg'
 import namesBandThumb from '../../assets/images/projects/names-band/000093060003.jpg'
 
 export default function Page(){
-    return <main className="flex sm:h-screen sm:w-screen flex-col items-center pt-24 bg-primary dark:bg-dark overflow-y-auto overflow-x-h-0">
+    return <main className="flex sm:h-screen sm:w-screen flex-col items-center pt-24 bg-primary dark:bg-dark overflow-y-auto overflow-x-hidden">
         <div className="h-screen w-screen font-superRetro italic flex flex-col sm:flex-row sm:items-start items-center w-full">
             <div className="sm:flex-col sm:block sm:w-1/2 sm:h-full">
                 <ProjectCard 
