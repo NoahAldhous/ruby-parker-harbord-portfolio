5364868b7ba8c2040472a04719cb80b66e58d978
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-13 17:35:48 +0100

    masonry grid created for engagements pathway

---
+++ project/src/assets/images/projects/engagements/export.tsx
@@ -0,0 +1,98 @@
+import engagements1 from './engagements1.jpg'
+import engagements2 from './engagements2.jpg'
+import engagements3 from './engagements3.jpg'
+import engagements4 from './engagements4.jpg'
+import engagements5 from './engagements5.jpg'
+import engagements6 from './engagements6.jpg'
+import engagements7 from './engagements7.jpg'
+import engagements8 from './engagements8.jpg'
+import engagements9 from './engagements9.jpg'
+import engagements10 from './engagements10.jpg'
+import engagements11 from './engagements11.jpg'
+import engagements12 from './engagements12.jpg'
+import engagements13 from './engagements13.jpg'
+import engagements14 from './engagements14.jpg'
+import engagements15 from './engagements15.jpg'
+import engagements16 from './engagements16.jpg'
+import engagements17 from './engagements17.jpg'
+import engagements18 from './engagements18.jpg'
+import engagements19 from './engagements19.jpg'
+
+export default [
+    {
+        alt: 'engagements1',
+        src: engagements1, 
+    },
+    {
+        alt: 'engagements2',
+        src: engagements2, 
+    },
+    {
+        alt: 'engagements3',
+        src: engagements3, 
+    },
+    {
+        alt: 'engagements4',
+        src: engagements4, 
+    },
+    {
+        alt: 'engagements5',
+        src: engagements5, 
+    },
+    {
+        alt: 'engagements6',
+        src: engagements6, 
+    },
+    {
+        alt: 'engagements7',
+        src: engagements7, 
+    },
+    {
+        alt: 'engagements8',
+        src: engagements8, 
+    },
+    {
+        alt: 'engagements9',
+        src: engagements9, 
+    },
+    {
+        alt: 'engagements10',
+        src: engagements10, 
+    },
+    {
+        alt: 'engagements11',
+        src: engagements11, 
+    },
+    {
+        alt: 'engagements12',
+        src: engagements12, 
+    },
+    {
+        alt: 'engagements13',
+        src: engagements13, 
+    },
+    {
+        alt: 'engagements14',
+        src: engagements14, 
+    },
+    {
+        alt: 'engagements15',
+        src: engagements15, 
+    },
+    {
+        alt: 'engagements16',
+        src: engagements16, 
+    },
+    {
+        alt: 'engagements17',
+        src: engagements17, 
+    },
+    {
+        alt: 'engagements18',
+        src: engagements18, 
+    },
+    {
+        alt: 'engagements19',
+        src: engagements19, 
+    },
+]
