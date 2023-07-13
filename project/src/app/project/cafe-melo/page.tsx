3b7ec0eb87a2730a35648d44bcb0cfcb5d80f8e9
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-13 14:08:28 +0100

    removed extra package and package-lock .json files that were accidentally created in folder above the main project folder. Now all dependencies are in one place

--- project/src/app/project/cafe-melo/page.tsx
+++ project/src/app/project/cafe-melo/page.tsx
@@ -1,5 +1,5 @@
 export default function Page(){
-    return <main className="flex min-h-screen flex-col items-center pt-24 bg-primary dark:bg-dark">
+    return <main className="flex min-h-screen flex-col items-center pt-24 px-6 bg-primary dark:bg-dark">
      <div className="flex flex-col items-center p-6 bg-red-500 w-full cursor-pointer">
         <div>Cafe Melo</div>
      </div>
