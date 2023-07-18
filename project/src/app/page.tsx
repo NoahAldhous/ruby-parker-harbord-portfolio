5d7f907f2812f6222b8ba62171e65ac68ee1636b
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-18 13:05:49 +0100

    updated styling on home page

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (  
     <main className="flex sm:h-screen sm:w-screen flex-col items-center pt-24 bg-primary dark:bg-dark overflow-y-auto overflow-x-hidden">
-      <div className="w-full object-contain flex flex-col sm:grid gap-6 sm:gap-4 sm:grid-cols-3 sm:grid-rows-3">
+      <div className="flex flex-col items-center p-2 sm:p-12 w-full">
         <MasonryGrid images={images}/>
       </div>
     </main>
