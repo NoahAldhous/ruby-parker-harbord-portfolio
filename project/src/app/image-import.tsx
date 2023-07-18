eca5a7f715a03b4111108e28fd2e0338c4933f71
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-18 14:39:00 +0100

    added thumbnails to home page

--- project/src/app/image-import.tsx
+++ project/src/app/image-import.tsx
@@ -1,32 +1,32 @@
 export default [
     {
         alt: 'home1',
-        src: '/home/home1.jpg',
+        src: '/home-page/home1.jpg',
         thumb: '/home/thumbnails/home-thumb.jpg',
     },
     {
         alt: 'home2',
-        src: '/home/home2.jpg',
+        src: '/home-page/home2.jpg',
         thumb: '/home/thumbnails/home-thumb.jpg',
     },
     {
         alt: 'home3',
-        src: '/home/home3.jpg',
+        src: '/home-page/home3.jpg',
         thumb: '/home/thumbnails/home-thumb.jpg', 
     },
     {
         alt: 'home4',
-        src: '/home/home4.jpg',
+        src: '/home-page/home4.jpg',
         thumb: '/home/thumbnails/home-thumb.jpg',
     },
     {
         alt: 'home5',
-        src: '/home/home5.jpg',
+        src: '/home-page/home5.jpg',
         thumb: '/home/thumbnails/home-thumb.jpg', 
     },
     {
         alt: 'home6',
-        src: '/home/home6.jpg',
+        src: '/home-page/home6.jpg',
         thumb: '/home/thumbnails/home-thumb.jpg',
     }
 ]
--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,16 +1,13 @@
 'use client';
 import MasonryGrid from './components/MasonryGrid';
 
-import Image from 'next/image';
-
 import images from './image-import'
 
 export default function Home() {
 
   return (  
     <main className="flex sm:h-screen sm:w-screen flex-col items-center pt-24 bg-primary dark:bg-dark overflow-y-auto overflow-x-hidden">
-      <div className="flex flex-col items-center p-2 sm:p-12 w-full">
-        
+      <div className="flex flex-col items-center p-2 sm:p-12 w-full">   
         <MasonryGrid images={images}/>
       </div>
     </main>
