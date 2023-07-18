e0107151749aa7bcda1128b999cbcd976cfc95eb
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-18 14:50:05 +0100

    added correct import path for home page images

--- project/src/app/image-import.tsx
+++ project/src/app/image-import.tsx
@@ -2,31 +2,31 @@ export default [
     {
         alt: 'home1',
         src: '/images/home-page/home1.jpg',
-        thumb: '/images/home/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
     },
     {
         alt: 'home2',
         src: '/images/home-page/home2.jpg',
-        thumb: '/images/home/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
     },
     {
         alt: 'home3',
         src: '/images/home-page/home3.jpg',
-        thumb: '/images/home/thumbnails/home-thumb.jpg', 
+        thumb: '/images/home-page/thumbnails/home-thumb.jpg', 
     },
     {
         alt: 'home4',
         src: '/images/home-page/home4.jpg',
-        thumb: '/images/home/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
     },
     {
         alt: 'home5',
         src: '/images/home-page/home5.jpg',
-        thumb: '/images/home/thumbnails/home-thumb.jpg', 
+        thumb: '/images/home-page/thumbnails/home-thumb.jpg', 
     },
     {
         alt: 'home6',
         src: '/images/home-page/home6.jpg',
-        thumb: '/images/home/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
     }
 ]
