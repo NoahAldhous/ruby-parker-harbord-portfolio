4a8a38d493b41bea83c68ca3ffeaa51f889decae
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-18 14:53:52 +0100

    added correct import path for home page images

--- project/src/app/image-import.tsx
+++ project/src/app/image-import.tsx
@@ -2,31 +2,31 @@ export default [
     {
         alt: 'home1',
         src: '/images/home-page/home1.jpg',
-        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb1.jpg',
     },
     {
         alt: 'home2',
         src: '/images/home-page/home2.jpg',
-        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb2.jpg',
     },
     {
         alt: 'home3',
         src: '/images/home-page/home3.jpg',
-        thumb: '/images/home-page/thumbnails/home-thumb.jpg', 
+        thumb: '/images/home-page/thumbnails/home-thumb3.jpg', 
     },
     {
         alt: 'home4',
         src: '/images/home-page/home4.jpg',
-        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb4.jpg',
     },
     {
         alt: 'home5',
         src: '/images/home-page/home5.jpg',
-        thumb: '/images/home-page/thumbnails/home-thumb.jpg', 
+        thumb: '/images/home-page/thumbnails/home-thumb5.jpg', 
     },
     {
         alt: 'home6',
         src: '/images/home-page/home6.jpg',
-        thumb: '/images/home-page/thumbnails/home-thumb.jpg',
+        thumb: '/images/home-page/thumbnails/home-thumb6.jpg',
     }
 ]
