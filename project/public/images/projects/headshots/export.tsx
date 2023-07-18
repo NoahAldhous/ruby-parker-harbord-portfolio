7d69782f91eb94d1e02dd532516f8d26551d34ee
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-18 13:01:53 +0100

    changed the import paths inside the public folder to reference from the root path

--- project/public/images/projects/headshots/export.tsx
+++ project/public/images/projects/headshots/export.tsx
@@ -1,13 +1,13 @@
-import headshots1 from './headshots1.jpg'
-import headshots2 from './headshots2.jpg'
-import headshots3 from './headshots3.jpg'
-import headshots4 from './headshots4.jpg'
-import headshots5 from './headshots5.jpg'
-import headshots6 from './headshots6.jpg'
-import headshots7 from './headshots7.jpg'
-import headshots8 from './headshots8.jpg'
-import headshots9 from './headshots9.jpg'
-import headshots10 from './headshots10.jpg'
+import headshots1 from '/images/projects/headshots/headshots1.jpg'
+import headshots2 from '/images/projects/headshots/headshots2.jpg'
+import headshots3 from '/images/projects/headshots/headshots3.jpg'
+import headshots4 from '/images/projects/headshots/headshots4.jpg'
+import headshots5 from '/images/projects/headshots/headshots5.jpg'
+import headshots6 from '/images/projects/headshots/headshots6.jpg'
+import headshots7 from '/images/projects/headshots/headshots7.jpg'
+import headshots8 from '/images/projects/headshots/headshots8.jpg'
+import headshots9 from '/images/projects/headshots/headshots9.jpg'
+import headshots10 from '/images/projects/headshots/headshots10.jpg'
 
 export default [
     {
