9802a5029be16315e360b52c1c1bcbbd8c3d4517
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-27 10:21:36 +0100

    navbar title text color changes on hover

--- project/src/app/components/NavigationBar.tsx
+++ project/src/app/components/NavigationBar.tsx
@@ -43,11 +43,11 @@ export default function NavigationBar() {
             <Link 
                 onClick={handleClose} 
                 key={'/'}
-                className='px-2 whitespace-nowrap'
+                className='px-2 whitespace-nowrap transition-all duration-600 md:hover:text-slate-500'
                 href={'/'}
             >
                 <motion.p
-                    className="font-superRetro italic text-md md:text-3xl align-middle w-1/2 items-center overflow-x-visible transition-all duration-600 hover:opacity-90"
+                    className="font-superRetro italic text-md md:text-3xl align-middle w-1/2 items-center overflow-x-visible"
                     initial='initialState'
                     animate='animateState'
                     exit='exitState'
