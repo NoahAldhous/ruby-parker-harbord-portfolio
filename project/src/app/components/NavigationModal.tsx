6e44dd7f068092bddeae9509bd2c9f1a4e8bfcff
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-27 09:41:12 +0100

    divided animation on navbar modal to two separate divs, one which animates whole element, the other animated the text

--- project/src/app/components/NavigationModal.tsx
+++ project/src/app/components/NavigationModal.tsx
@@ -24,44 +24,58 @@ export default function NavigationModal({children, isOpen, handleClose}: Navigat
     return (
         <ReactPortal wrapperId="react-portal-modal-container" >
                 <>
-                    <div className="md:hidden flex flex-col justify-center items-center fixed top-0 left-0 w-screen h-screen z-40 bg-primary dark:bg-dark opacity-90">
-                        <div className="fixed rounded flex flex-col justify-start items-center box-border min-w-fit h-5/6 overflow-hidden p-5">
+                    <motion.div 
+                        className="md:hidden flex flex-col justify-center items-center fixed top-0 left-0 w-screen h-screen z-40 bg-primary dark:bg-dark opacity-90"
+                        initial='initialState'
+                        animate='animateState'
+                        exit='exitState'
+                        transition={{
+                            duration: 0.5,
+                            delay: 0.2
+                        }}
+                        variants={{
+                            initialState: {
+                                opacity:0
+                            },
+                            animateState: {
+                                opacity: 1
+                            },
+                            exitState: {
+                                opacity: 0
+                            },
+                        }}
+                        >
+                        <motion.div 
+                            className="fixed rounded flex flex-col justify-start items-center box-border min-w-fit h-5/6 overflow-hidden p-5"
+                            initial='initialState'
+                            animate='animateState'
+                            exit='exitState'
+                            transition={{
+                                duration: 0.5,
+                                delay: 0.2
+                            }}
+                            variants={{
+                                initialState: {
+                                    y:20
+                                },
+                                animateState: {
+                                    y:0
+                                },
+                                exitState: {
+                                    y:0
+                                },
+                            }}>
                             <button 
                                 onClick={handleClose} 
                                 className="py-2 px-8 self-end"
                             >
                                 Close
                             </button>
-                            <AnimatePresence mode='wait'>
-                                <motion.div 
-                                    className="flex justify-center items-center"
-                                    initial='initialState'
-                                animate='animateState'
-                                exit='exitState'
-                                transition={{
-                                    duration: 0.5,
-                                    delay: 0.3
-                                }}
-                                variants={{
-                                    initialState: {
-                                        opacity:0,
-                                        y:20
-                                    },
-                                    animateState: {
-                                        opacity: 1,
-                                        y:0
-                                    },
-                                    exitState: {
-                                        opacity: 0,
-                                        y:0
-                                    },
-                                }}
-                                > 
-                                    {children}
-                                </motion.div>
-                            </AnimatePresence>
-                        </div>
-                    </div>
+                            <div className="flex justify-center items-center"> 
+                                {children}
+                            </div>
+                        </motion.div>
+                    </motion.div>
                 </>
         </ReactPortal>
     )
