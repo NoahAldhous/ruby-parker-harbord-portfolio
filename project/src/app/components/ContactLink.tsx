5c2ca96af7f9a430018b546d3562c7330791f7c8
NoahAldhous <naldhous@hotmail.co.uk>
2023-07-27 12:07:45 +0100

    added animation to individual contact Link components

--- project/src/app/components/ContactLink.tsx
+++ project/src/app/components/ContactLink.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 interface ContactLinkProps {
     firstLineText: string;
@@ -7,8 +8,25 @@ interface ContactLinkProps {
 }
 
 export default function ContactLink({firstLineText, secondLineText, url}: ContactLinkProps){
-    return <div>    
+    return <motion.div
+                initial='initialState'
+                animate='animateState'
+                transition={{
+                    duration: 0.3,
+                    delay: 0.2
+                }}
+                variants={{
+                    initialState: {
+                        x:-15,
+                        opacity:0
+                    },
+                    animateState: {
+                        x:0,
+                        opacity: 0.9
+                    }
+                }}
+    >    
         <p className="text-sm sm:text-lg md:text-2xl">{firstLineText}</p>
         <Link className="font-superRetro italic underline text-md sm:text-3xl md:text-6xl align-middle tracking-wider transition-all duration-600 sm:hover:text-slate-600 sm:hover:pl-4" href={url} target="_blank">{secondLineText}</Link>
-    </div>
+    </motion.div>
 }
