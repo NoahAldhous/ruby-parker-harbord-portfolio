e6c488105054a14f76c3de7b5c649f8392059c7e
NoahAldhous <naldhous@hotmail.co.uk>
2023-08-08 18:36:18 +0100

    added responsive resizing to contacts page

--- project/src/app/components/ContactLink.tsx
+++ project/src/app/components/ContactLink.tsx
@@ -10,7 +10,7 @@ interface ContactLinkProps {
 
 export default function ContactLink({firstLineText, secondLineText, url}: ContactLinkProps){
     return <div>    
-        <p className="text-md font-superRetro italic sm:text-lg md:text-2xl opacity-90">{firstLineText}</p>
-        <Link className="font-superRetro italic text-3xl sm:text-3xl md:text-6xl align-middle tracking-wider transition-all duration-600 sm:opacity-90 sm:hover:opacity-100 sm:hover:pl-4" href={url} target="_blank">{secondLineText}</Link>
+        <p className="text-md font-superRetro italic sm:text-lg md:text-xl lg:text-2xl opacity-90">{firstLineText}</p>
+        <Link className="font-superRetro italic text-3xl sm:text-3xl md:text-4xl lg:text-6xl align-middle tracking-wider transition-all duration-600 lg:opacity-90 lg:hover:opacity-100 hover:pl-4" href={url} target="_blank">{secondLineText}</Link>
     </div>
 }
