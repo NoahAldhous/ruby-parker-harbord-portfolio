f82899d72cc3d662342f013d2737d7a8f6d352c3
NoahAldhous <naldhous@hotmail.co.uk>
2023-08-15 12:08:24 +0100

    ali project page created and project link on project page created

--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -2,41 +2,49 @@
 import ProjectCard from '../components/NewProjectCard';
 
 //image imports
-import cafeMeloThumb from '../../../public/images/projects/cafe-melo/melo1.jpg';
-import engagementsThumb from '../../../public/images/projects/engagements/engagements1.jpg';
-import headshotsThumb from '../../../public/images/projects/headshots/headshots1.jpg'
-import namesBandThumb from '../../../public/images/projects/names-band/names1.jpg'
+import cafeMelo from '../../../public/images/projects/cafe-melo/melo1.jpg';
+import engagements from '../../../public/images/projects/engagements/engagements1.jpg';
+import headshots from '../../../public/images/projects/headshots/headshots1.jpg'
+import namesBand from '../../../public/images/projects/names-band/names1.jpg'
+import ali from '../../../public/images/projects/ali/ali1.jpg'
 
 export default function Page(){
     return <main className="flex flex-col items-center pt-24 bg-primary dark:bg-dark overflow-x-hidden box-border">
         <div className="w-screen font-superRetro italic flex flex-col sm:flex-row sm:items-start items-center mb-12 overflow-x-hidden overflow-y-hidden">
             <div className="justify-center flex items-center flex-col w-2/3 sm:w-1/2 sm:h-full">
                 <ProjectCard
-                    imageSource={cafeMeloThumb} 
+                    imageSource={cafeMelo} 
                     projectTitle='cafe melo' 
                     imageAlt='woman drinking from glass'
                     url='cafe-melo'
                 />
                 <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
                 <ProjectCard 
-                    imageSource={engagementsThumb} 
+                    imageSource={engagements} 
                     projectTitle='engagements' 
                     imageAlt='woman drinking from glass'
                     url='engagements'
+                />
+                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
+                <ProjectCard 
+                    imageSource={ali} 
+                    projectTitle='ali' 
+                    imageAlt='portrait of a man'
+                    url='ali'
                 />
                 <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
             </div>
             <div className="justify-center flex items-center flex-col w-2/3 sm:w-1/2 sm:h-full">
                 <div className="hidden sm:inline sm:my-1/5 sm:h-1/2 sm:w-1/2 invisible"></div>
                 <ProjectCard 
-                    imageSource={headshotsThumb} 
+                    imageSource={headshots} 
                     projectTitle='headshots' 
                     imageAlt='headshot of a woman'
                     url='headshots'
                 />
                 <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
                 <ProjectCard 
-                    imageSource={namesBandThumb} 
+                    imageSource={namesBand} 
                     projectTitle='names' 
                     imageAlt='headshot of a woman'
                     url='names-band'
