69d0b9fc1f212883e8eeca121bac5c921458e51c
NoahAldhous <naldhous@hotmail.co.uk>
2023-08-16 11:07:42 +0100

    added new home page images

--- project/src/app/image-import.tsx
+++ project/src/app/image-import.tsx
@@ -4,6 +4,14 @@ import home3 from '../../public/images/home-page/home3.jpg'
 import home4 from '../../public/images/home-page/home4.jpg'
 import home5 from '../../public/images/home-page/home5.jpg'
 import home6 from '../../public/images/home-page/home6.jpg'
+import home7 from '../../public/images/home-page/home7.jpg'
+import home8 from '../../public/images/home-page/home8.jpg'
+import home9 from '../../public/images/home-page/home9.jpg'
+import home10 from '../../public/images/home-page/home10.jpg'
+import home11 from '../../public/images/home-page/home11.jpg'
+import home12 from '../../public/images/home-page/home12.jpg'
+import home13 from '../../public/images/home-page/home13.jpg'
+import home14 from '../../public/images/home-page/home14.jpg'
 
 export default [
     {
@@ -41,5 +49,53 @@ export default [
         src: '/images/home-page/home6.jpg',
         staticImageData: home6,
         thumb: '/images/home-page/thumbnails/home-thumb6.jpg',
-    }
+    },
+    {
+        alt: 'home7',
+        src: '/images/home-page/home7.jpg',
+        staticImageData: home7,
+        thumb: '/images/home-page/thumbnails/home-thumb7.jpg'
+    },
+    {
+        alt: 'home8',
+        src: '/images/home-page/home8.jpg',
+        staticImageData: home8,
+        thumb: '/images/home-page/thumbnails/home-thumb8.jpg'
+    },
+    {
+        alt: 'home9',
+        src: '/images/home-page/home9.jpg',
+        staticImageData: home9,
+        thumb: '/images/home-page/thumbnails/home-thumb9.jpg' 
+    },
+    {
+        alt: 'home10',
+        src: '/images/home-page/home10.jpg',
+        staticImageData: home10,
+        thumb: '/images/home-page/thumbnails/home-thumb10.jpg' 
+    },
+    {
+        alt: 'home11',
+        src: '/images/home-page/home11.jpg',
+        staticImageData: home11,
+        thumb: '/images/home-page/thumbnails/home-thumb11.jpg' 
+    },
+    {
+        alt: 'home12',
+        src: '/images/home-page/home12.jpg',
+        staticImageData: home12,
+        thumb: '/images/home-page/thumbnails/home-thumb12.jpg' 
+    },
+    {
+        alt: 'home13',
+        src: '/images/home-page/home13.jpg',
+        staticImageData: home13,
+        thumb: '/images/home-page/thumbnails/home-thumb13.jpg' 
+    },
+    {
+        alt: 'home14',
+        src: '/images/home-page/home14.jpg',
+        staticImageData: home14,
+        thumb: '/images/home-page/thumbnails/home-thumb14.jpg' 
+    },
 ]
