950defb415fd710de890575affbc833e01f422e3
NoahAldhous <naldhous@hotmail.co.uk>
2023-08-18 23:17:04 +0100

    added text in about page

--- project/src/app/about/page.tsx
+++ project/src/app/about/page.tsx
@@ -47,40 +47,51 @@ export default function Page(){
                 />
         </motion.div>
         <section className='flex flex-col w-10/12 sm:w-2/3 md:w-2/3 lg:w-1/2 text-xs sm:text-xs md:text-base lg:text-base xl:text-lg'>
-        <motion.div
-                className='w-full'
-                initial='initialState'
-                whileInView='inViewState'
-                exit='exitState'
-                viewport={{ once: true }}
-                transition={{
-                    duration: 0.7,
-                    delay: 0.1
-                }}
-                variants={{
-                    initialState: {
-                        opacity:0,
-                        x:15
-                    },
-                    inViewState: {
-                        opacity: 1,
-                        x:0
-                    },
-                    exitState: {
-                        opacity: 0,
-                        x:15
-                    },
-                }}
-                >
-            <h2 className='font-superRetro italic text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
-                Placeholder
-            </h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus, tortor id laoreet molestie, urna risus blandit est, at aliquet lorem justo id nisi. Nam et ultrices nunc. Integer eget sapien auctor, gravida nulla id, pellentesque tortor. Aliquam eu fringilla felis. Aliquam erat volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec et massa non mi pulvinar malesuada eu non eros. 
-            <br/>
-            <br/>
-            Praesent lobortis tortor non tristique pellentesque. Donec a accumsan massa. Mauris eu est venenatis, tempus dolor vitae, pellentesque diam. Praesent sem lacus, dapibus id hendrerit eget, dignissim vel nulla. Etiam in tellus vel est mattis pulvinar. Etiam bibendum gravida magna, et sollicitudin est aliquet non. Aenean nec nibh vel magna placerat hendrerit a sed libero. Nullam orci turpis, sollicitudin sed urna quis, egestas egestas sapien.
-            </p>
+            <motion.div
+                    className='w-full'
+                    initial='initialState'
+                    whileInView='inViewState'
+                    exit='exitState'
+                    viewport={{ once: true }}
+                    transition={{
+                        duration: 0.7,
+                        delay: 0.1
+                    }}
+                    variants={{
+                        initialState: {
+                            opacity:0,
+                            x:15
+                        },
+                        inViewState: {
+                            opacity: 1,
+                            x:0
+                        },
+                        exitState: {
+                            opacity: 0,
+                            x:15
+                        },
+                    }}
+            >
+                <h2 className='font-superRetro italic text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
+                    Hello and Welcome!
+                </h2>
+                <p>
+                Thanks for being here!
+                <br/>
+                I'm Ruby. I'm a Photographer, Writer and Director based in London.
+                <br/>
+
+                I have always enjoyed photography and before freelancing professionally in this field, I worked primarily in Film and Television. I love to tell stories with visuals and to explore what a picture can say.
+                <br/>
+
+                I have worked in a variety of different environments, including Behind the Scenes photography, Weddings, Brand work and Red Carpet premieres. I have a cinematic documentary style and love to capture people in their most natural state. 
+                <br/>
+
+                Whilst I am proficient in all types of photography, shooting on film is my favourite way of recording these moments. I've been doing film photography for almost a decade now. It's timeless and adds an organic look which is perfect for street photography and rustic weddings.
+                <br/>
+
+                If you're looking for a photographer for an event, headshots, branding or even just to get some nice pictures for yourself or someone you love, please don't hesitate to get in touch and we can discuss further! 
+                </p>
             </motion.div>
         </section>
      </section>
