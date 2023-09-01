71f04545b95d4fc6f2eaae9ff0a892d3baae2970
NoahAldhous <naldhous@hotmail.co.uk>
2023-09-01 11:34:57 +0100

    the year in the footer is now updated dynamically through the getFullYear function

--- project/src/app/components/Footer.tsx
+++ project/src/app/components/Footer.tsx
@@ -2,13 +2,16 @@
 
 import Link from 'next/link';
 
-export default function ContactLink(){
+export default function Footer(){
+
+    const currentYear:number = new Date().getFullYear()
+
     return <div className='w-screen flex flex-col sm:flex-row items-center justify-around mb-4 text-xs sm:text-xs md:text-xs lg:text-sm xl:text-base'>
         <section className='sm:w-1/3 flex justify-center'>
             <Link className="" href='https://www.instagram.com/rubyparkerh/' target="_blank">@rubyparkerh</Link>
         </section>  
         <section className='sm:w-1/3 flex justify-center'>
-            <p className="text-md">&#169; Ruby Parker-Harbord 2023</p>
+            <p className="text-md">&#169; Ruby Parker-Harbord {currentYear}</p>
         </section>  
         <section className='sm:w-1/3 flex justify-center'>
             <Link className="" href='mailto:rubyparkerphotography@gmail.com' target="_blank">rubyparkerphotography@gmail.com</Link>
