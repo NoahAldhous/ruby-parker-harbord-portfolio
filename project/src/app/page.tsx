8f4033359f889e9fa824b18d7ce1e342bac11276
NoahAldhous <naldhous@hotmail.co.uk>
2023-09-01 14:10:01 +0100

    new home page images added, and thumbnails for the lightgallery

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -5,19 +5,19 @@ import { motion, AnimatePresence } from 'framer-motion';
 import Footer from '../app/components/Footer'
 
 //image import
-import home1 from '../../public/images/film/film7.jpg';
-import home2 from '../../public/images/home-page/home18.jpg';
-import home3 from '../../public/images/film/film64.jpg';
-import home4 from '../../public/images/projects/engagements/engagements9.jpg';
-import home5 from '../../public/images/film/film9.jpg';
-import home6 from '../../public/images/film/film37.jpg';
-import home7 from '../../public/images/home-page/home19.jpg';
-import home8 from '../../public/images/home-page/home4.jpg';
-import home9 from '../../public/images/projects/names-band/names4.jpg';
-import home10 from '../../public/images/home-page/home11.jpg';
-import home11 from '../../public/images/home-page/home15.jpg';
-import home12 from '../../public/images/home-page/home16.jpg';
-import home13 from '../../public/images/home-page/home17.jpg';
+import home1 from '../../public/images/home-page/home1.jpg';
+import home2 from '../../public/images/home-page/home2.jpg';
+import home3 from '../../public/images/home-page/home3.jpg';
+import home4 from '../../public/images/home-page/home4.jpg';
+import home5 from '../../public/images/home-page/home5.jpg';
+import home6 from '../../public/images/home-page/home6.jpg';
+import home7 from '../../public/images/home-page/home7.jpg';
+import home8 from '../../public/images/home-page/home8.jpg';
+import home9 from '../../public/images/home-page/home9.jpg';
+import home10 from '../../public/images/home-page/home10.jpg';
+import home11 from '../../public/images/home-page/home11.jpg';
+import home12 from '../../public/images/home-page/home12.jpg';
+import home13 from '../../public/images/home-page/home13.jpg';
 
 //light gallery
 import type {LightGallery} from 'lightgallery/lightgallery';
@@ -34,57 +34,57 @@ import lgThumbnail from 'lightgallery/plugins/thumbnail';
 export default function homepage() {
 
   const images = [
+    {
+      src:  '/images/home-page/home2.jpg',
+      thumb:  '/images/home-page/thumbnails/homethumb2.jpg'
+    },
   {
-    src:  '/images/film/film7.jpg',
-    thumb:  '/images/film/film7.jpg'
+    src:  '/images/home-page/home1.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb1.jpg'
   },
   {
-    src:  '/images/home-page/home18.jpg',
-    thumb:  '/images/home-page/home18.jpg'
-  },
-  {
-    src:  '/images/film/film64.jpg',
-    thumb:  '/images/film/film64.jpg'
-  },
-  {
-    src:  '/images/projects/engagements/engagements9.jpg',
-    thumb:  '/images/projects/engagements/engagements9.jpg'
-  },
-  {
-    src:  '/images/film/film37.jpg',
-    thumb:  '/images/film/film37.jpg'
-  },
-  {
-    src:  '/images/film/film9.jpg',
-    thumb:  '/images/film/film9.jpg'
-  },
-  {
-    src:  '/images/home-page/home19.jpg',
-    thumb:  '/images/home-page/home19.jpg'
+    src:  '/images/home-page/home3.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb3.jpg'
   },
   {
     src:  '/images/home-page/home4.jpg',
-    thumb:  '/images/home-page/home4.jpg'
+    thumb:  '/images/home-page/thumbnails/homethumb4.jpg'
   },
   {
-    src:  '/images/projects/names-band/names4.jpg',
-    thumb:  '/images/projects/names-band/names4.jpg'
+    src:  '/images/home-page/home6.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb6.jpg'
   },
   {
-    src: 'images/home-page/home11.jpg',
-    thumb: '/images/home-page/home11.jpg'
+    src:  '/images/home-page/home5.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb5.jpg'
   },
   {
-    src: '/images/home-page/home15.jpg',
-    thumb: '/images/home-page/home15.jpg'
+    src:  '/images/home-page/home7.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb7.jpg'
   },
   {
-    src: '/images/home-page/home16.jpg',
-    thumb: '/images/home-page/home16.jpg'
+    src:  '/images/home-page/home8.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb8.jpg'
   },
   {
-    src: '/images/home-page/home17.jpg',
-    thumb: '/images/home-page/home17.jpg'
+    src:  '/images/home-page/home9.jpg',
+    thumb:  '/images/home-page/thumbnails/homethumb9.jpg'
+  },
+  {
+    src: 'images/home-page/home10.jpg',
+    thumb: '/images/home-page/thumbnails/homethumb10.jpg'
+  },
+  {
+    src: '/images/home-page/home11.jpg',
+    thumb: '/images/home-page/thumbnails/homethumb11.jpg'
+  },
+  {
+    src: '/images/home-page/home12.jpg',
+    thumb: '/images/home-page/thumbnails/homethumb12.jpg'
+  },
+  {
+    src: '/images/home-page/home13.jpg',
+    thumb: '/images/home-page/thumbnails/homethumb13.jpg'
   },
   ]
 
@@ -125,7 +125,7 @@ export default function homepage() {
               <Image
                 priority={true}
                 onClick={() => {
-                    lightboxRef.current?.openGallery(0);
+                    lightboxRef.current?.openGallery(1);
                 }}
                 placeholder='blur' 
                 src={home1} 
@@ -148,7 +148,7 @@ export default function homepage() {
               <Image
                 priority={true}
                 onClick={() => {
-                    lightboxRef.current?.openGallery(1);
+                    lightboxRef.current?.openGallery(0);
                 }}
                 placeholder='blur' 
                 src={home2} 
