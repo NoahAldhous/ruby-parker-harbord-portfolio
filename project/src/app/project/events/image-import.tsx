0c20bcbc1c084ea5c35453a74cdc2234a294e41b
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-10 14:28:23 +0000

    new events photos added, name of events page changed to events and theatre

--- project/src/app/project/events/image-import.tsx
+++ project/src/app/project/events/image-import.tsx
@@ -13,20 +13,46 @@ import events12 from '../../../../public/images/projects/events/events12.jpg'
 import events13 from '../../../../public/images/projects/events/events13.jpg'
 import events14 from '../../../../public/images/projects/events/events14.jpg'
 import events15 from '../../../../public/images/projects/events/events15.jpg'
-
+import events16 from '../../../../public/images/projects/events/events16.jpg'
+import events17 from '../../../../public/images/projects/events/events17.jpg'
+import events18 from '../../../../public/images/projects/events/events18.jpg'
+import events19 from '../../../../public/images/projects/events/events19.jpg'
+import events20 from '../../../../public/images/projects/events/events20.jpg'
+import events21 from '../../../../public/images/projects/events/events21.jpg'
+import events22 from '../../../../public/images/projects/events/events22.jpg'
+import events23 from '../../../../public/images/projects/events/events23.jpg'
+import events24 from '../../../../public/images/projects/events/events24.jpg'
+import events25 from '../../../../public/images/projects/events/events25.jpg'
+import events26 from '../../../../public/images/projects/events/events26.jpg'
+import events27 from '../../../../public/images/projects/events/events27.jpg'
+import events28 from '../../../../public/images/projects/events/events28.jpg'
+import events29 from '../../../../public/images/projects/events/events29.jpg'
+import events30 from '../../../../public/images/projects/events/events30.jpg'
+import events31 from '../../../../public/images/projects/events/events31.jpg'
+import events32 from '../../../../public/images/projects/events/events32.jpg'
+import events33 from '../../../../public/images/projects/events/events33.jpg'
+import events34 from '../../../../public/images/projects/events/events34.jpg'
+import events35 from '../../../../public/images/projects/events/events35.jpg'
+import events36 from '../../../../public/images/projects/events/events36.jpg'
+import events37 from '../../../../public/images/projects/events/events37.jpg'
+import events38 from '../../../../public/images/projects/events/events38.jpg'
+import events39 from '../../../../public/images/projects/events/events39.jpg'
+import events40 from '../../../../public/images/projects/events/events40.jpg'
+import events41 from '../../../../public/images/projects/events/events41.jpg'
+import events42 from '../../../../public/images/projects/events/events42.jpg'
 
 export default [
     {
         alt: 'events1',
         src: '/images/projects/events/events1.jpg',
         staticImageData: events1,
-        thumb: '/images/projects/events/thumbnails/events-thumb1.jpg' 
+        thumb: '/images/projects/events/thumbnails/events-thumb1.jpg'
     },
     {
         alt: 'events2',
         src: '/images/projects/events/events2.jpg',
         staticImageData: events2,
-        thumb: '/images/projects/events/thumbnails/events-thumb2.jpg' 
+        thumb: '/images/projects/events/thumbnails/events-thumb2.jpg'
     },
     {
         alt: 'events3',
@@ -38,7 +64,7 @@ export default [
         alt: 'events4',
         src: '/images/projects/events/events4.jpg',
         staticImageData: events4,
-        thumb: '/images/projects/events/thumbnails/events-thumb4.jpg' 
+        thumb: '/images/projects/events/thumbnails/events-thumb4.jpg'
     },
     {
         alt: 'events5',
@@ -50,19 +76,19 @@ export default [
         alt: 'events6',
         src: '/images/projects/events/events6.jpg',
         staticImageData: events6,
-        thumb: '/images/projects/events/thumbnails/events-thumb6.jpg' 
+        thumb: '/images/projects/events/thumbnails/events-thumb6.jpg'
     },
     {
         alt: 'events7',
         src: '/images/projects/events/events7.jpg',
         staticImageData: events7,
-        thumb: '/images/projects/events/thumbnails/events-thumb7.jpg' 
+        thumb: '/images/projects/events/thumbnails/events-thumb7.jpg'
     },
     {
         alt: 'events8',
         src: '/images/projects/events/events8.jpg',
         staticImageData: events8,
-        thumb: '/images/projects/events/thumbnails/events-thumb8.jpg' 
+        thumb: '/images/projects/events/thumbnails/events-thumb8.jpg'
     },
     {
         alt: 'events9',
@@ -106,4 +132,166 @@ export default [
         staticImageData: events15,
         thumb: '/images/projects/events/thumbnails/events-thumb15.jpg' 
     },
+    {
+        alt: 'events16',
+        src: '/images/projects/events/events16.jpg',
+        staticImageData: events16,
+        thumb: '/images/projects/events/thumbnails/events-thumb16.jpg' 
+    },
+    {
+        alt: 'events17',
+        src: '/images/projects/events/events17.jpg',
+        staticImageData: events17,
+        thumb: '/images/projects/events/thumbnails/events-thumb17.jpg' 
+    },
+    {
+        alt: 'events18',
+        src: '/images/projects/events/events18.jpg',
+        staticImageData: events18,
+        thumb: '/images/projects/events/thumbnails/events-thumb18.jpg' 
+    },
+    {
+        alt: 'events19',
+        src: '/images/projects/events/events19.jpg',
+        staticImageData: events19,
+        thumb: '/images/projects/events/thumbnails/events-thumb19.jpg' 
+    },
+    {
+        alt: 'events20',
+        src: '/images/projects/events/events20.jpg',
+        staticImageData: events20,
+        thumb: '/images/projects/events/thumbnails/events-thumb20.jpg' 
+    },
+    {
+        alt: 'events21',
+        src: '/images/projects/events/events21.jpg',
+        staticImageData: events21,
+        thumb: '/images/projects/events/thumbnails/events-thumb21.jpg' 
+    },
+    {
+        alt: 'events22',
+        src: '/images/projects/events/events22.jpg',
+        staticImageData: events22,
+        thumb: '/images/projects/events/thumbnails/events-thumb22.jpg' 
+    },
+    {
+        alt: 'events23',
+        src: '/images/projects/events/events23.jpg',
+        staticImageData: events23,
+        thumb: '/images/projects/events/thumbnails/events-thumb23.jpg' 
+    },
+    {
+        alt: 'events24',
+        src: '/images/projects/events/events24.jpg',
+        staticImageData: events24,
+        thumb: '/images/projects/events/thumbnails/events-thumb24.jpg' 
+    },
+    {
+        alt: 'events25',
+        src: '/images/projects/events/events25.jpg',
+        staticImageData: events25,
+        thumb: '/images/projects/events/thumbnails/events-thumb25.jpg' 
+    },
+    {
+        alt: 'events26',
+        src: '/images/projects/events/events26.jpg',
+        staticImageData: events26,
+        thumb: '/images/projects/events/thumbnails/events-thumb26.jpg' 
+    },
+    {
+        alt: 'events27',
+        src: '/images/projects/events/events27.jpg',
+        staticImageData: events27,
+        thumb: '/images/projects/events/thumbnails/events-thumb27.jpg' 
+    },
+    {
+        alt: 'events28',
+        src: '/images/projects/events/events28.jpg',
+        staticImageData: events28,
+        thumb: '/images/projects/events/thumbnails/events-thumb28.jpg' 
+    },
+    {
+        alt: 'events29',
+        src: '/images/projects/events/events29.jpg',
+        staticImageData: events29,
+        thumb: '/images/projects/events/thumbnails/events-thumb29.jpg' 
+    },
+    {
+        alt: 'events30',
+        src: '/images/projects/events/events30.jpg',
+        staticImageData: events30,
+        thumb: '/images/projects/events/thumbnails/events-thumb30.jpg' 
+    },
+    {
+        alt: 'events31',
+        src: '/images/projects/events/events31.jpg',
+        staticImageData: events31,
+        thumb: '/images/projects/events/thumbnails/events-thumb31.jpg' 
+    },
+    {
+        alt: 'events32',
+        src: '/images/projects/events/events32.jpg',
+        staticImageData: events32,
+        thumb: '/images/projects/events/thumbnails/events-thumb32.jpg' 
+    },
+    {
+        alt: 'events33',
+        src: '/images/projects/events/events33.jpg',
+        staticImageData: events33,
+        thumb: '/images/projects/events/thumbnails/events-thumb33.jpg' 
+    },
+    {
+        alt: 'events34',
+        src: '/images/projects/events/events34.jpg',
+        staticImageData: events34,
+        thumb: '/images/projects/events/thumbnails/events-thumb34.jpg' 
+    },
+    {
+        alt: 'events35',
+        src: '/images/projects/events/events35.jpg',
+        staticImageData: events35,
+        thumb: '/images/projects/events/thumbnails/events-thumb35.jpg' 
+    },
+    {
+        alt: 'events36',
+        src: '/images/projects/events/events36.jpg',
+        staticImageData: events36,
+        thumb: '/images/projects/events/thumbnails/events-thumb36.jpg' 
+    },
+    {
+        alt: 'events37',
+        src: '/images/projects/events/events37.jpg',
+        staticImageData: events37,
+        thumb: '/images/projects/events/thumbnails/events-thumb37.jpg' 
+    },
+    {
+        alt: 'events38',
+        src: '/images/projects/events/events38.jpg',
+        staticImageData: events38,
+        thumb: '/images/projects/events/thumbnails/events-thumb38.jpg' 
+    },
+    {
+        alt: 'events39',
+        src: '/images/projects/events/events39.jpg',
+        staticImageData: events39,
+        thumb: '/images/projects/events/thumbnails/events-thumb39.jpg' 
+    },
+    {
+        alt: 'events40',
+        src: '/images/projects/events/events40.jpg',
+        staticImageData: events40,
+        thumb: '/images/projects/events/thumbnails/events-thumb40.jpg' 
+    },
+    {
+        alt: 'events41',
+        src: '/images/projects/events/events41.jpg',
+        staticImageData: events41,
+        thumb: '/images/projects/events/thumbnails/events-thumb41.jpg' 
+    },
+    {
+        alt: 'events42',
+        src: '/images/projects/events/events42.jpg',
+        staticImageData: events42,
+        thumb: '/images/projects/events/thumbnails/events-thumb42.jpg' 
+    }
 ]
