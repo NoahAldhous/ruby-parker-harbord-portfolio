98a098555c322140d1a0dab478dad68a83a9565f
NoahAldhous <naldhous@hotmail.co.uk>
2024-01-10 14:36:52 +0000

    changed the order that the project previews appear on the project page

--- project/src/app/project/page.tsx
+++ project/src/app/project/page.tsx
@@ -17,44 +17,6 @@ export default function Page(){
     return <main className="flex flex-col items-center pt-24 bg-primary dark:bg-dark overflow-x-hidden box-border">
         <div className="w-screen font-superRetro italic flex flex-col sm:flex-row sm:items-start items-center mb-12 overflow-x-hidden overflow-y-hidden">
             <div className="justify-center flex items-center flex-col w-2/3 sm:w-1/2 sm:h-full">
-                <ProjectCard
-                    imageSource={engagements} 
-                    projectTitle='engagements' 
-                    imageAlt='a happy couple'
-                    url='engagements'
-                />
-                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
-                <ProjectCard 
-                    imageSource={namesBand} 
-                    projectTitle='names' 
-                    imageAlt='people from a band'
-                    url='names-band'
-                />
-                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
-                <ProjectCard 
-                    imageSource={portraits} 
-                    projectTitle='portraits' 
-                    imageAlt='portrait of a man'
-                    url='portraits'
-                />
-                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
-                <ProjectCard 
-                    imageSource={summer} 
-                    projectTitle='summer scott' 
-                    imageAlt='woman standing amongst trees'
-                    url='summer-scott'
-                />
-                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
-                <ProjectCard 
-                    imageSource={events} 
-                    projectTitle='events' 
-                    imageAlt='woman standing amongst trees'
-                    url='events'
-                />
-                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
-            </div>
-            <div className="justify-center flex items-center flex-col w-2/3 sm:w-1/2 sm:h-full">
-                <div className="hidden sm:inline sm:my-1/5 sm:h-1/2 sm:w-1/2 invisible"></div>
                 <ProjectCard 
                     imageSource={carpet} 
                     projectTitle='red carpets' 
@@ -69,18 +31,52 @@ export default function Page(){
                     url='headshots'
                 />
                 <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
+                <ProjectCard
+                    imageSource={engagements} 
+                    projectTitle='engagements' 
+                    imageAlt='a happy couple'
+                    url='engagements'
+                />
+                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
+                <ProjectCard 
+                    imageSource={namesBand} 
+                    projectTitle='Music Artists' 
+                    imageAlt='people from a band'
+                    // change the url page name to more generic e.g 'band' or 'music artists'
+                    url='names-band'
+                />
+                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
+            </div>
+            {/* SECOND COLUMN */}
+            <div className="justify-center flex items-center flex-col w-2/3 sm:w-1/2 sm:h-full">
+                <div className="hidden sm:inline sm:my-1/5 sm:h-1/2 sm:w-1/2 invisible"></div>
+                <ProjectCard 
+                    imageSource={events} 
+                    projectTitle='events and theatre' 
+                    imageAlt='a drag queen performance'
+                    url='events'
+                />
+                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
+                <ProjectCard 
+                //will be changed to BTS stills
+                    imageSource={summer} 
+                    projectTitle='summer scott' 
+                    imageAlt='woman standing amongst trees'
+                    url='summer-scott'
+                />
+                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
+                <ProjectCard 
+                    imageSource={portraits} 
+                    projectTitle='portraits' 
+                    imageAlt='portrait of a man'
+                    url='portraits'
+                />
+                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
                 <ProjectCard 
                     imageSource={cafeMelo} 
                     projectTitle='cafe melo' 
                     imageAlt='cup of coffee'
                     url='cafe-melo'
-                />
-                <div className="my-1/5 h-1/2 w-1/2 invisible"></div>
-                <ProjectCard 
-                    imageSource={patient} 
-                    projectTitle='patient is a verb' 
-                    imageAlt='person wearing boxing gloves on a stage'
-                    url='patient-is-a-verb'
                 />
             </div>
         </div>
